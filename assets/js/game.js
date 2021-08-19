59b288ca14c46db875f5a9a4ca3f1f38d39e7897
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-08-18 20:47:17 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,3 +76,56 @@ for(var i= 0; i < enemyNames.length; i++) {
         break;
     }
 }
+
+//funtion to start a new game
+var startGame = function() {
+    // resets player's stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for (var i=0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i+1));
+
+            var pickedEnemyName = enemyNames[i];
+
+            enemyHealth = 50
+
+            fight(pickedEnemyName)
+        }
+        else {
+            window.alert("you have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    
+    endGame();
+};
+
+// start the game when the page loads
+startGame();
+
+// funtion to end entire game
+var endGame = function() {
+    // If player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great iob, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play agin?");
+
+    if (playerAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+endGame();
